1988ed38bec7f4a9c43319d26dba717e07846487
vitalii-hamash <vitalii.hamash@uvik.net>
2022-01-01 17:33:36 +0200

    finishing_home_page

--- src/data.js
+++ src/data.js
@@ -67,7 +67,7 @@ export const popularProducts = [
     },
     {
         id:7,
-        img:"https://www.vintageindustries.nl/download_front/qympzk1762/2217_Arrow_Jacket_Forest.png",
+        img:"https://images.unsplash.com/flagged/photo-1585052201332-b8c0ce30972f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80",
     },
     {
         id:8,
